fbb23ce0748ecba5ed828da96de719770259f915
Raman <Raman@pollub.edu.pl>
2023-10-03 17:04:21 +0200

    feat1: Added new function

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
     }
+
+    public void NewFunction1()
+    {
+        System.out.println("Nowa funkcja 1!");
+    }
 }
